da48bdfe35224ba2fbb415594cd5aee1319b8e78
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-12-11 21:33:31 +0600

    cards are added in business section

--- project_hoobank-main/src/components/Business.jsx
+++ project_hoobank-main/src/components/Business.jsx
@@ -2,6 +2,19 @@ import styles, {layout} from "../style"
 import { features } from "../constants";
 import  Button from './Button';
 
+const FeatureCard = ({icon, title, content, index}) =>(
+  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
+    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+      <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain"/>
+    </div>
+
+    <div className="flex-1 flex flex-col ml-3">
+      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1">{title}</h4>
+      <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px] mb-1">{content}</p>
+    </div>
+  </div>
+)
+
 const Business = () =>(
     <section id="features" className={layout.section}>
       <div className={layout.sectionInfo}>
@@ -9,6 +22,12 @@ const Business = () =>(
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio quod nulla explicabo porro. Id quisquam illum numquam ex consequatur eveniet?</p>
 
         <Button styles="mt-10"/>
+      </div>
+
+      <div>
+        {features.map((feature, index) =>(
+          <FeatureCard key={feature.id} {...feature} index={index}/>
+        ))}
       </div>
     </section>
 )
