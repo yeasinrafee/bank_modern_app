8f03b472a2561a8f527eefa34c53aaf05c4a82fe
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-12-11 23:13:32 +0600

    buttons of billing section are added

--- project_hoobank-main/src/components/Billing.jsx
+++ project_hoobank-main/src/components/Billing.jsx
@@ -13,6 +13,11 @@ const Billing = () =>(
     <div className={layout.sectionInfo}>
       <h2 className={styles.heading2}>Easily control your <br className='sm:block hidden'/> billing & invoicing.</h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non ducimus quos molestias aliquid odit ratione.</p>
+
+      <div className='flex flex-row flex-wrap sm:mt-10 mt-6'>
+        <img src={apple} alt="apple_store" className='w-[128px] h-[42px] object-contain mr-5 cursor-pointer' />
+        <img src={google} alt="google_playstore" className='w-[128px] h-[42px] object-contain mr-5 cursor-pointer' />
+      </div>
     </div>
   </section>
 )
