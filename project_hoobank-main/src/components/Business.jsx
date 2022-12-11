3b119c5c429a190b3d55e3f6cabea9de02aa9e81
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-12-11 17:25:16 +0600

    heading and texts of business section is added

--- project_hoobank-main/src/components/Business.jsx
+++ project_hoobank-main/src/components/Business.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import styles, {layout} from "../style"
+import { features } from "../constants";
+import  Button from './Button';
 
-const Business = () => {
-  return (
-    <div>Business</div>
-  )
-}
+const Business = () =>(
+    <section id="features" className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>You do the business, <br className="sm:block hidden"/> we'll handle the money.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio quod nulla explicabo porro. Id quisquam illum numquam ex consequatur eveniet?</p>
+
+        <Button styles="mt-10"/>
+      </div>
+    </section>
+)
 
 export default Business
